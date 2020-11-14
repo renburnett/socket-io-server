6770a048372be8053259fc819a36aa19fb4e4644
renburnett <renardburnett@gmail.com>
2020-11-14 11:38:32 -0800

    more tweaks lol

--- server.js
+++ server.js
@@ -2,25 +2,20 @@ const express = require('express');
 const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
+app.get('/', (req, res, next) => {res.send('<p> socket.io server running. . . </p>')})
+
 const server = app.listen(4000, () => {
-    console.info(`listening for requests on port ${PORT}`);
+    console.info(`listening for requests on port 4000`);
 });
 
-// const io = socket(server, {
-//     cors: true,
-//     transports: ['polling'],
-//     origins: ['http://192.168.0.12:*', "http://localhost:*", "https://sazzle-server.herokuapp.com/*", 'https://sazzle-server.herokuapp.com/socket.io/'],
-//    });
-
-   const io = socket(server, {
+const io = socket(server, {
     cors: true,
-    transports: ['websocket'],
-    origins: ['http://192.168.0.12:*', "http://localhost:*", "https://sazzle-server.herokuapp.com/*", 'https://sazzle-server.herokuapp.com/socket.io/'],
-   });
+    transports: ['polling'],
+    origins: ['http://192.168.0.12:*', 'http://localhost:*', 'https://sazzle-server.herokuapp.com/*', 'https://sazzle-server.herokuapp.com/socket.io/'],
+});
 
 io.on('connection', (socket) => {
     console.info('socket connection successful', socket.id);
